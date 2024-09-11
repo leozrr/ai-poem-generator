ffd36a21b4636b2ee2a9ebe96b0f27c87e70f61f
leozrr <121227252+leozrr@users.noreply.github.com>
2024-09-11 16:15:44 +0200

    updated to a recipe

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 20,
+    delay: 1,
   });
 }
 
-function generatePoem(event) {
+function generateRecipe(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "atb7e03426c9eba5578b1o9acbffe81a";
-  let prompt = `User instructions: Generate a korean and english poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate an easy but fancy asian food recipe of ${instructionsInput.value}`;
   let context =
-    "You are a Poem expert and love to write short poems. Your mission is to generate a four line poem and separate each line with a <br />. Do not name html in the beginning. Make sure to follow the user instructions. Sign it with SheCodes AI in <strong> element at the end of the poem and in a separate line.";
+    "You are a Gastronomy expert and love to cook asian recipes. Your mission is to generate an easy but also fancy recipe including the ingredients the user instruct as well as other ingredients that fit to the instructions. Make sure to follow the user instructions. Include the name of the recipe as the headline and in <strong> element at the beginning of the recipe and use one < /br> afterwards. Separate the ingredients list from the steps with < /br>. Include two < /br> after the list of ingredients.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⌛ Just a second. I am generating the poem about ${instructionsInput.value} for you...</div>`;
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⌛ Just a second. I am generating the recipe with the mentioned ingredients for you...</div>`;
 
-  axios.get(apiUrl).then(displayPoem);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
-let poemFormElement = document.querySelector("#form-poem-generator");
-poemFormElement.addEventListener("submit", generatePoem);
+let recipeFormElement = document.querySelector("#form-recipe-generator");
+recipeFormElement.addEventListener("submit", generateRecipe);
